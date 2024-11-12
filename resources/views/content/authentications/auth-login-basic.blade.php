d4c4d543d047b6b3ceaf3601a6f821d9fe8b4f4b
yishak <yishakab24@gmail.com>
2024-11-12 15:31:46 +0300

    fix and change

--- resources/views/content/authentications/auth-login-basic.blade.php
+++ resources/views/content/authentications/auth-login-basic.blade.php
@@ -65,13 +65,6 @@
                     </div>
                 </div>
                 <!-- /Login -->
-                <img src="{{ asset('assets/img/illustrations/tree-3.png') }}" alt="auth-tree"
-                    class="authentication-image-object-left d-none d-lg-block">
-                <img src="{{ asset('assets/img/illustrations/auth-basic-mask-light.png') }}"
-                    class="authentication-image d-none d-lg-block" height="172" alt="triangle-bg">
-                <img src="{{ asset('assets/img/illustrations/tree.png') }}" alt="auth-tree"
-                    class="authentication-image-object-right d-none d-lg-block">
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
